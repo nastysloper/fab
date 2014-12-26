e6f4914be84f80c66bd33ad83e42e9da79a71ef3
Rich Vogt <richvogtjr@gmail.com>
2014-12-25 18:21:18 -0600

    import os, check empty file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 # returns <type 'int'> the exit code.
 # output = subprocess.call(['git', 'status', '--porcelain'])
@@ -12,7 +13,9 @@ task = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE
 for l in task.stdout.readlines():
    print "This file is not committed. ==> %s" % (l.split()[1])
 
+# print type(task.stdout)
+
 # for l in task.stdout.readlines():
    # print "This file is not committed. ==> %s" % (l)
-
-print("Please commit, checkout, or stash your changes.")
+if os.path.getsize("/Users/richvogt/Code/git_test") > 0:
+  print "Please commit, checkout, or stash your changes."
