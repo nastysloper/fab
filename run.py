d7d1dd666fb5e05aa3d1b15a1967e5351f1f0d0d
Rich Vogt <richvogtjr@gmail.com>
2014-12-27 20:30:22 -0600

    add cd

--- run.py
+++ run.py
@@ -1,18 +1,12 @@
 import subprocess
 import os
 
-# returns <type 'int'> the exit code.
-# output = subprocess.call(['git', 'status', '--porcelain'])
-
-# print type(output)
-# print str(output)
-# subprocess.call(['ls', '-la'])
 
 task = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
 flag = False
 for l in task.stdout.readlines():
   flag = True
-  print "This file is not committed. ==> %s" % (l.split()[1])
+  print "This file is not committed. ==> %s" % l.split()[1]
 
 if flag:
   print("Please commit, checkout, or stash your changes.")
@@ -23,5 +17,9 @@ else:
   merge = subprocess.Popen(['git', 'merge', '--ff-only'])
   fetch.communicate([0])
 
-# for l in task.stdout.readlines():
-   # print "This file is not committed. ==> %s" % (l)
+print "\nNow running cd"
+current = os.getcwd()
+print "we are in %s" % current
+os.chdir("/Users/richvogt/Code")
+subprocess.call("ls")
+# returns back to current directory
